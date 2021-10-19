fbfb162da8e2b0259dcc7fb4efb8af267380d978
Elijah <elijahmylcev@gmail.com>
2021-10-19 23:06:51 +0300

    Preparing the markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import '@babel/polyfill'
+
+const root = document.getElementById('#app')
+const newDiv = document.createElement('div')
+let start = newDiv = null
+const adress = 'https:\/\/randomfox.ca\/images\/92.jpg'
+newDiv.innerHTML = `
+    <div class="wrapper">
+        <div class="wrapper__img">
+            <img class="wrapper__img_photo" src=${adress} alt="Photo">
+        </div>
+        <button class="wrapper__btn">Get Photo</button>
+    </div>
+`
+root.insertBefore(newDiv, start)
+
+import './style.scss'
