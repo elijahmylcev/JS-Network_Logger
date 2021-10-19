6fbba5e00f03fab6bb6a59209d2f91033ccd35ce
Elijah <elijahmylcev@gmail.com>
2021-10-19 23:40:30 +0300

    refactoring template

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import '@babel/polyfill'
+import './style.scss'
 
 const adress = 'https:\/\/randomfox.ca\/images\/92.jpg'
+let newDiv = document.createElement('div')
 
 function getElement(adress) {
-  let newDiv = document.createElement('div')
 
-  newDiv.innerHTML = `
-<div class="wrapper">
-    <div class="wrapper__img">
-        <img class="wrapper__img_photo" src=${adress} alt="Photo">
+  if (!adress || adress == '') {
+    newDiv.innerHTML = `
+      <div class = "fail">OOOps!
+        <button class="fail__btn">Get Photo Fox</button>
+      </div>
+    `
+  } else {
+    newDiv.innerHTML = `
+    <div class="wrapper">
+        <div class="wrapper__img">
+            <img class="wrapper__img_photo" src=${adress} alt="Photo">
+        </div>
+        <button class="wrapper__btn">Get Photo Fox</button>
     </div>
-    <button class="wrapper__btn">Get Photo</button>
-</div>
-`
-const start = document.querySelector('.start')
-const parentDiv = start.parentNode
+    `
+  }
+
+  const start = document.querySelector('.start')
+  const parentDiv = start.parentNode
   parentDiv.insertBefore(newDiv, start)
 }
 
 getElement(adress)
-import './style.scss'
