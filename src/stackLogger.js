6695b138decd603b5e83b6e88db8f30ef815aeac
Elijah <elijahmylcev@gmail.com>
2021-10-22 00:18:51 +0300

    refact logger

--- src/stackLogger.js
+++ src/stackLogger.js
@@ -2,7 +2,8 @@ let oldXHROpen = window.XMLHttpRequest.prototype.open
 const stackForLinkes = []
 window.XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
     this.addEventListener('load', function () {
-        stackForLinkes.push(`load: ${JSON.parse(this.response)}`);
+        stackForLinkes.push(JSON.parse(this.response));
+        console.log(stackForLinkes);
     })
 
     return oldXHROpen.apply(this, arguments)
