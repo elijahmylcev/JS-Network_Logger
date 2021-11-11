5c1e41b8325bb8667fddf3847c2e92ea2b63e226
Elijah <elijahmylcev@gmail.com>
2021-11-11 20:15:10 +0300

    Create file function

--- server/server.js
+++ server/server.js
@@ -2,15 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
+const fs = require('fs')
+const stackLogger = [];
 
 const PORT = 3001;
 
 app.use(bodyParser.json(), cors({
-    origin: 'http://localhost:4200'
-}), (req,res) => {
-    console.log(req.body);
+  origin: 'http://localhost:4200'
+}), (req, res) => {
+  stackLogger.push(req.body)
+  console.log(stackLogger);
+  sorter(stackLogger);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server start...`);
+  console.log(`Server start...`);
 })
+
+function sorter(arr) {
+  if (arr.length >= 3) {
+    fs.appendFile('statistic.txt', `${arr}`, function (err) {
+      if (err) {
+        throw err
+      }
+      console.log('saved');
+    })
+  }
+}
